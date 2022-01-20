d5d0a7530d534e357fcea50ce5fbd67249fd6929
olhavaraOB <rastislav.olhava@open-bean.com>
2022-01-20 16:39:04 +0100

    Algorithmic part.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cmath>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -57,77 +58,98 @@ void null_under(vector< vector<double> > &Ab, const int upper_row, const int lef
     }
 }
 
-vector<double> provide_the_only_solution(vector< vector<double> > &Ab) {
-    int n = Ab.size();
+vector<double> provide_one_solution(const vector< vector<double> > & upper, int dim, vector<int> pivots) {
+    int n = upper.size();
+    int m = n-dim;
+    vector<double> line(m+1,0);
+    vector<vector<double>> temp(m, line);
 
-    vector<double> solution(n);
-    for (int i=n-1; i>=0; i--) {
-        solution[i] = Ab[i][n] / Ab[i][i];
-        for (int k=i-1;k>=0; k--) {
-            Ab[k][n] -= Ab[k][i] * solution[i];
+    for (int col=0; col<m; col++) {
+        for (int row=0; row<m; row++) {
+            temp[row][col] = upper[row][pivots[col]];
         }
     }
+    for (int row=0; row<m; row++) {
+        temp[row][m] = upper[row][n];
+    }
+
+
+    vector<double> partial_solution(m);
+    for (int i=m-1; i>=0; i--) {
+        partial_solution[i] = temp[i][m] / temp[i][i];
+        for (int k=i-1;k>=0; k--) {
+            temp[k][m] -= temp[k][i] * partial_solution[i];
+        }
+    }
+    vector<double> solution(n,0);
+    for (int i=0; i< m; i++) { solution[pivots[i]] = partial_solution[i];}
+
     return solution;
 }
 
 
-int frobenius(const vector<vector<double>> Ab) {
+bool frobenius(const vector<vector<double>> Ab, int dim) {
     int n = Ab.size();
-    int row = n-1;
-    int dim = 0;
 
-    bool null_row;
-    while (row > -1) {
-        null_row = true;
-        for (int col=row; col < n; col++) {
-            if (Ab[row][col] != 0) {
-                null_row = false;
-                break;
-            }
-        }
-        if (null_row) {
-            dim++;
-        } else {
-            break;}
-        row--;
-    }
-    for (row=n-1; row > n-1-dim; row--) {
+    for (int row=n-1; row > n-1-dim; row--) {
         if (Ab[row][n] != 0) {
-            dim = -1;
+            return false;
         }
     }
 
-    return dim;
+    return true;
 }
 
-vector<vector<double>> get_upper(vector<vector<double>> Ab) {
+tuple<vector<vector<double>>, vector<int>> get_upper(vector<vector<double>> Ab) {
     int n = Ab.size();
     int cur_row = 0;
+    vector<int> pivots;
 
     for (int cur_col=0; cur_col < n; cur_col++) {
-        int max_row = find_max_under(Ab, cur_col, cur_col);
+        int max_row = find_max_under(Ab, cur_row, cur_col);
         if (Ab[max_row][cur_col] == 0 ) {
             continue;}
         if (max_row != cur_row){
             swap_rows(Ab, cur_row, max_row);
         }
         null_under(Ab, cur_row, cur_col);
+        pivots.push_back(cur_col);
         cur_row++;
     }
 
-    return Ab;
+
+    return make_tuple(Ab, pivots);
 }
+
+
+vector<vector<double>> find_base(vector<vector<double>> upper, int dim, vector<int> pivots) {
+    int n = upper.size();
+    vector<double> line(n);
+    vector<vector<double>> base;
+    vector<vector<double>> temp;
+    vector<double> partial_solution;
+    int col = 0;
+    for (int row=0; row<dim; row++) {
+        while (binary_search(pivots.begin(), pivots.end(), col)) {col++;}
+        temp = upper;
+        for (int row_=0; row_<n- dim; row_++) {temp[row_][n] = -upper[row_][col];}
+        base.push_back(provide_one_solution(temp, dim,pivots));
+        base[row][col] = 1;
+        col++;
+    }
+
+//    for (int sol_no=0; sol_no<dim; sol_no++) {
 //
-//vector<int> find_loose(vector<vector<double>> upper, int dim) {
-//    vector<int> loose(dim);
-//    int n = upper.size();
-//
-//
+//        for (int i=n-1; i>-1; i--) {
+//            partial_solution[i] = temp[i][m] / temp[i][i];
+//            for (int k=i-1;k>=0; k--) {
+//                temp[k][m] -= temp[k][i] * partial_solution[i];
+//            }
+//        }
 //    }
-//    return vector<int>();
-//}
 
-
+    return base;
+}
 
 int main() {
     int n;
@@ -150,44 +172,50 @@ int main() {
     print(Ab);
 
     std::tuple<int, double> a;
+    vector<vector<double>> upper;
+    vector<int> pivots;
+    tie(upper, pivots)= get_upper(Ab);
 
-    vector<vector<double>> upper = get_upper(Ab);
+    int dimension;
+    dimension = n - pivots.size();
 
-    int dimension = frobenius(upper);
 
-    if (dimension == -1) {
+    if (!frobenius(upper, dimension)) {
         cout << "There is no solution. " ;
         return 0;
     }
-    if (dimension == 0) {
 
-        vector<double> x(n);
-        x = provide_the_only_solution(upper);
-
-        cout << "Result:\t";
-        for (int i=0; i<n; i++) {
-            cout << x[i] << " ";
-        }
-        cout << endl;
-        return 0;
-    }
+    vector<double> x(n);
+    vector<vector<double>> base;
+    x = provide_one_solution(upper,dimension, pivots);
 
     if (dimension > 0) {
-
-
-        vector<double> x(n);
-        vector<int> loose_indices(dimension);
-
-//        loose_indices = find_loose(upper, dimension);
-        x = provide_the_only_solution(upper);
-
-        cout << "Result:\t";
-        for (int i=0; i<n; i++) {
-            cout << x[i] << " ";
-        }
-        cout << endl;
-        return 0;
+        base = find_base(upper, dimension, pivots);
     }
+
+    cout << "Result:\t";
+    cout << "K={[";
+    for (int i=0; i<n; i++) {
+        if (i<n-1) {cout << x[i] << " ";}
+        else {cout << x[i] << "]";}
+    }
+    if (dimension == 0) { cout << "}";}
+    else {
+        cout << " + <";
+        for (int s=0; s<dimension; s++) {
+            cout << "[";
+            for (int i=0; i<n; i++) {
+                if (i<n-1) {cout << base[s][i] << " ";}
+                else {cout << base[s][i] << "]";}
+            }
+            if (s != dimension-1) {cout << ", ";}
+        }
+        cout << ">";
+    }
+    cout << "}" << endl;
+    return 0;
+
+
 
 
 }
